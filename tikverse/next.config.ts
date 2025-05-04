fac98fe12e5ae01352cea96a3b19e7856576b598
Ranjit <ranjitgupta2002@gmail.com>
2025-05-04 15:10:13 +0530

    cleaned code, installed packages, added rules

--- tikverse/next.config.ts
+++ tikverse/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config, { isServer }) => {
+    // Add a rule to handle the canvas.node binary module
+    config.module?.rules.push({ test:/\.node$/, use: "node-loader" })
+
+    // Exclude canvas from being processed by Next.js in the browser
+    if (!isServer) config.externals.push('canvas');
+    return config;
+
+  }
 };
 
 export default nextConfig;
